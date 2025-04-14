cefcb57444820ea094dc3e5d3ca27fd096819e96
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-04-14 19:46:34 +0300

    Exit Update
    Çıkış yapıldığında login değerinin 0 olması sağlandı.
    Anasayfaya yönlendirme sağlandı.
    Hata çıktıları verildi.

--- src/users/exit.php
+++ src/users/exit.php
@@ -1,3 +1,25 @@
 <?php
+    session_start();
     include('../../config.php');
+    if(isset($_SESSION['tcno']))
+    {
+        $tcno = $_SESSION['tcno'];
+        $update_sql = "UPDATE users SET login = 0 WHERE tcno = '$tcno'";
+        if(mysqli_query($mysqlB, $update_sql))
+        {
+            session_unset();
+            session_destroy();
+            header("Location: ../index.php");
+            exit();
+        }
+        else
+        {
+            echo "Bir hata oluştu. Lütfen tekrar deneyin.";
+        }
+    }
+    else
+    {
+        header("Location: ../index.php");
+        exit();
+    }
 ?>
