229eb24f4d7a52184d72d5203d79be5112eee658
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-04-16 15:01:04 +0300

    PHP Mail
    Mail gönderme kısmına ait kodlar düzenlendi.

--- src/users/authentication.php
+++ src/users/authentication.php
@@ -1,67 +1,67 @@
 <?php
-require_once '../PHPMailer-master/src/Exception.php';
-require_once '../PHPMailer-master/src/PHPMailer.php';
-require_once '../PHPMailer-master/src/SMTP.php';
-include('../../config.php');
-session_start();
-$success_message = '';
-$error_message = '';
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    $email = mysqli_real_escape_string($mysqlB, $_POST['email']);
-    if(filter_var($email, FILTER_VALIDATE_EMAIL))
+    require_once '../PHPMailer-master/src/Exception.php';
+    require_once '../PHPMailer-master/src/PHPMailer.php';
+    require_once '../PHPMailer-master/src/SMTP.php';
+    include('../../config.php');
+    session_start();
+    $success_message = '';
+    $error_message = '';
+    if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        $sql = "SELECT * FROM users WHERE email='$email'";
-        $result = mysqli_query($mysqlB, $sql);
-        if(mysqli_num_rows($result) > 0)
+        $email = mysqli_real_escape_string($mysqlB, $_POST['email']);
+        if(filter_var($email, FILTER_VALIDATE_EMAIL))
         {
-            $user = mysqli_fetch_assoc($result);
-            $reset_token = bin2hex(random_bytes(32));
-            date_default_timezone_set('Europe/Istanbul');
-            $token_expiry = date('Y-m-d H:i:s', strtotime('+1 hour'));
-            $update_sql = "UPDATE users SET reset_token='$reset_token', token_expiry='$token_expiry' WHERE email='$email'";
-            if(mysqli_query($mysqlB, $update_sql))
+            $sql = "SELECT * FROM users WHERE email='$email'";
+            $result = mysqli_query($mysqlB, $sql);
+            if(mysqli_num_rows($result) > 0)
             {
-                $reset_link = "http://localhost/DivingLog/src/users/reset_password.php?token=$reset_token";
-                $mail = new PHPMailer\PHPMailer\PHPMailer();
-                $mail->isSMTP();
-                $mail->Host = 'smtp.gmail.com';
-                $mail->SMTPAuth = true;
-                $mail->Username = ($email);
-                $mail->Password = 'yaef dgtn euzd dtkj';
-                $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Port = 587;
-                $mail->setFrom('divinglog@example.com', 'DivingLog');
-                $mail->addAddress($email);
-                $mail->CharSet = 'UTF-8';
-                $mail->Subject = "=?UTF-8?B?" . base64_encode('Şifre Sıfırlama Talebi') . "?=";
-                $mail->Body = "Şifrenizi sıfırlamak için aşağıdaki bağlantıya tıklayın:\n$reset_link";
-                $mail->isHTML(false);
-                $mail->SMTPDebug = 0;
-                if($mail->send())
+                $user = mysqli_fetch_assoc($result);
+                $reset_token = bin2hex(random_bytes(32));
+                date_default_timezone_set('Europe/Istanbul');
+                $token_expiry = date('Y-m-d H:i:s', strtotime('+1 hour'));
+                $update_sql = "UPDATE users SET reset_token='$reset_token', token_expiry='$token_expiry' WHERE email='$email'";
+                if(mysqli_query($mysqlB, $update_sql))
                 {
-                    $success_message = "Şifre sıfırlama bağlantınız e-posta adresinize gönderildi.";
+                    $reset_link = "http://localhost/DivingLog/src/users/reset_password.php?token=$reset_token";
+                    $mail = new PHPMailer\PHPMailer\PHPMailer();
+                    $mail->isSMTP();
+                    $mail->Host = 'smtp.gmail.com';
+                    $mail->SMTPAuth = true;
+                    $mail->Username = ($email);
+                    $mail->Password = 'yaef dgtn euzd dtkj';
+                    $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
+                    $mail->Port = 587;
+                    $mail->setFrom('divinglog@example.com', 'DivingLog');
+                    $mail->addAddress($email);
+                    $mail->CharSet = 'UTF-8';
+                    $mail->Subject = "=?UTF-8?B?" . base64_encode('Şifre Sıfırlama Talebi') . "?=";
+                    $mail->Body = "Şifrenizi sıfırlamak için aşağıdaki bağlantıya tıklayın:\n$reset_link";
+                    $mail->isHTML(false);
+                    $mail->SMTPDebug = 0;
+                    if($mail->send())
+                    {
+                        $success_message = "Şifre sıfırlama bağlantınız e-posta adresinize gönderildi.";
+                    }
+                    else
+                    {
+                        $error_message = "E-posta gönderilemedi. Lütfen tekrar deneyin. Hata: " . $mail->ErrorInfo;
+                    }
                 }
                 else
                 {
-                    $error_message = "E-posta gönderilemedi. Lütfen tekrar deneyin. Hata: " . $mail->ErrorInfo;
+                    $error_message = "Veritabanı güncellenirken bir hata oluştu.";
                 }
             }
             else
             {
-                $error_message = "Veritabanı güncellenirken bir hata oluştu.";
+                $error_message = "Bu e-posta adresiyle kayıtlı bir kullanıcı bulunamadı.";
             }
         }
         else
         {
-            $error_message = "Bu e-posta adresiyle kayıtlı bir kullanıcı bulunamadı.";
+            $error_message = "Geçersiz e-posta adresi.";
         }
     }
-    else
-    {
-        $error_message = "Geçersiz e-posta adresi.";
-    }
-}
 ?>
 
 <!DOCTYPE html>
