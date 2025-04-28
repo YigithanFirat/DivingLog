1f158b86c540dc51e1bff934fa5dc20160fba3fc
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-04-28 16:50:53 +0300

    Edit Diving Plan Export PDF
    • Hatalı fontlar düzeltildi.

--- src/admin/edit_diving_plan_export_pdf.php
+++ src/admin/edit_diving_plan_export_pdf.php
@@ -21,14 +21,14 @@
     $pdf->SetAuthor('DivingLog Uygulaması');
     $pdf->SetTitle('Dalış Planı');
     $pdf->SetHeaderData('', 0, 'Dalış Planı Raporu', 'Tarih: ' . date('d.m.Y'));
-    $pdf->setHeaderFont(Array('helvetica', '', 12));
-    $pdf->setFooterFont(Array('helvetica', '', 10));
+    $pdf->setHeaderFont(Array('dejavusans', '', 12));
+    $pdf->setFooterFont(Array('dejavusans', '', 10));
     $pdf->SetMargins(15, 27, 15);
     $pdf->SetHeaderMargin(10);
     $pdf->SetFooterMargin(10);
     $pdf->SetAutoPageBreak(TRUE, 20);
     $pdf->AddPage();
-    $pdf->SetFont('dejavusans', '', 12);
+    $pdf->SetFont('dejavusans', '', 10);
     $html = '<h2 style="text-align:center;">Dalış Planı Detayları</h2>';
     $html .= '<table border="1" cellpadding="6" cellspacing="0" style="width:100%;">';
     foreach($row as $key => $value) 
