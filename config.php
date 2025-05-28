6a2857d5b3c8414ee90888a81f345b74881b4405
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-05-28 20:59:55 +0300

    Config
    Bağlantı bilgileri güncellendi.

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php
-    $hostname = "localhost";
-    $user = "root";
-    $password = "[priadon1.5]";
-    $database = "divinglog";
-    $mysqlB = mysqli_connect($hostname, $user, $password, $database);
-    if(!$mysqlB)
-    {
-        die("Veritabanı bağlantısı başarısız! Lütfen daha sonra tekrar deneyin.");
-    }
-    mysqli_set_charset($mysqlB, "utf8mb4");
+$hostname = "localhost";
+$user = "root";
+$password = "";
+$database = "divinglog";
+
+$mysqlB = mysqli_connect($hostname, $user, $password, $database);
+
+if (!$mysqlB) {
+    die("Veritabanı bağlantısı başarısız! Lütfen daha sonra tekrar deneyin.");
+}
+
+mysqli_set_charset($mysqlB, "utf8mb4");
 ?>
