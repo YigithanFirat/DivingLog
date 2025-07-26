08a905cd05a3aec65b09d28f48f46f5b75d9f00b
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-07-26 13:06:11 +0300

    Certificate List
    Iconlar eklendi.
    Kullanıcı ibaresi kaldırıldı Öğrenci ibareleri eklendi.

--- src/admin/certificate_list.php
+++ src/admin/certificate_list.php
@@ -43,17 +43,17 @@ $result = mysqli_query($mysqlB, $query);
                 <h2>Admin Panel</h2>
             </div>
             <ul>
-                <li><a href="../index.php">Ana Sayfa</a></li>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="manage_users.php">Kullanıcıları Yönet</a></li>
-                <li><a href="diving.php">Dalış Oluştur</a></li>
-                <li><a href="manage_diving.php">Dalışları Yönet</a></li>
-                <li><a href="diving_place.php">Dalış Bölgeleri</a></li>
-                <li><a href="certificate.php">Sertifika Oluştur</a></li>
-                <li><a href="certificate_list.php">Sertifikaları Listele</a></li>
-                <li><a href="health_inspection.php">Sağlık Raporu Oluştur</a></li>
-                <li><a href="health_inspection_list.php">Sağlık Raporlarını Listele</a></li>
-                <li><a href="../users/exit.php">Çıkış Yap</a></li>
+                <li><a href="../index.php"><i class="fas fa-home"></i> Ana Sayfa</a></li>
+                <li><a href="dashboard.php"><i class="fas fa-chart-line"></i> Dashboard</a></li>
+                <li><a href="manage_users.php"><i class="fas fa-users"></i> Öğrenci Listesi</a></li>
+                <li><a href="diving.php"><i class="fas fa-water"></i> Dalış Oluştur</a></li>
+                <li><a href="manage_diving.php"><i class="fas fa-database"></i> Dalışları Yönet</a></li>
+                <li><a href="diving_place.php"><i class="fas fa-map-marker-alt"></i> Dalış Bölgeleri</a></li>
+                <li><a href="certificate.php"><i class="fas fa-certificate"></i> Sertifika Oluştur</a></li>
+                <li><a href="certificate_list.php"><i class="fas fa-list"></i> Sertifikaları Listele</a></li>
+                <li><a href="health_inspection.php"><i class="fas fa-notes-medical"></i> Sağlık Raporu Oluştur</a></li>
+                <li><a href="health_inspection_list.php"><i class="fas fa-clipboard-list"></i> Sağlık Raporlarını Listele</a></li>
+                <li><a href="../users/exit.php"><i class="fas fa-sign-out-alt"></i> Çıkış Yap</a></li>
             </ul>
         </div>
 
