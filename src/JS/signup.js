fc10d44fd8032bc73055477957cab1c011326d1e
Yiğithan Fırat <103052898+YigithanFirat@users.noreply.github.com>
2025-07-26 13:11:41 +0300

    Signup JS
    • ToggleSidebar fonksiyonu eklendi.
    • Şifre uyuşmazlığı eklendi.

--- src/JS/signup.js
+++ src/JS/signup.js
@@ -2,11 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("form");
     const sifre = document.getElementById("sifre");
     const tekrarsifre = document.getElementById("tekrarsifre");
+    const errorDiv = document.getElementById("form-errors");
 
     form.addEventListener("submit", function (e) {
         if (sifre.value !== tekrarsifre.value) {
             e.preventDefault();
-            alert("Şifreler uyuşmuyor. Lütfen iki alanın da aynı olduğundan emin olun.");
+            // Hata mesajını div içine yaz
+            errorDiv.innerHTML = '<ul><li>Şifreler uyuşmuyor. Lütfen iki alanın da aynı olduğundan emin olun.</li></ul>';
+            // Gerekirse inputlara odaklan
+            tekrarsifre.focus();
         }
     });
 });
+
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('hidden');
+}
